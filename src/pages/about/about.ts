090d8747cd6b7dfcba3860304537e37b8f2fdea9
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2017-09-21 00:48:38 -0400

    get central position and remove tabs

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import geolib from 'geolib';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +9,40 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    navCtrl: NavController,
+    platform: Platform,
+    private geolocation: Geolocation
+  ) {
+    platform.ready()
+      .then(() => this.getCurrentPosition())
+      .then(coords => this.getCentralPosition(coords));
   }
 
+  getCurrentPosition() {
+    return this.geolocation.getCurrentPosition()
+      .then(({ coords }) => {
+        console.log('Got current position!', coords);
+        return { latitude: coords.latitude, longitude: coords.longitude };
+      })
+      .catch(error => {
+        console.log('Error getting geolocation', error);
+        return error;
+      });
+  }
+
+  getCentralPosition(coords: { latitude: number, longitude: number }) {
+    const locations = [
+      coords,
+      { latitude: 25.992046, longitude: -80.283645 },
+      { latitude: 25.942871, longitude: -80.123380 }
+    ];
+
+    const centerOfBounds = geolib.getCenterOfBounds(locations);
+    const center = geolib.getCenter(locations);
+
+
+    console.log('Got center of bounds position!', centerOfBounds);
+    console.log('Got center position!', center);
+  }
 }
