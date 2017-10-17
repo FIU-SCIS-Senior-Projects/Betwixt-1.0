f3feadae46f6087205d0f9faebf650702c2d7bbf
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-10-16 22:10:53 -0400

    Add options to the workfrom places

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,12 +1,10 @@
 var crypto = require('crypto');
 var group_socket = require('../server');
 
-exports.create_group = (req, res) =>  {
-    //Randomly generated 5 bytes. Encoded in base64.
-    var uid = crypto.randomBytes(4).toString('hex');
-    res.send(JSON.stringify({uid : uid}));
-}
+exports.create_group = (req, res) => {
+  //Randomly generated 5 bytes. Encoded in base64.
+  var uid = crypto.randomBytes(4).toString('hex');
+  res.send(JSON.stringify({ uid }));
+};
 
-exports.join_group = (req,res) => {
-    
-}
+exports.join_group = (req, res) => {};
