1b1d1615b4b55e94e268a8743514c216a01c3591
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-10-18 23:56:54 -0400

    add select location socket events

--- sockets/groupSocket.js
+++ sockets/groupSocket.js
@@ -1,32 +1,42 @@
-module.exports = (io) => {
+module.exports = io => {
+  return {
+    create: () => {
+      console.log('Group socket started');
 
-    return {
-        create: () => {
+      //Whenever someone connects this gets executed
+      io.on('connection', function(socket) {
+        console.log('Connection made.');
 
-            console.log('Group socket started');
+        socket.on('joinGroup', userInfo => {
+          console.log(`User joined group: ${userInfo.groupUID}`);
+          socket.join(userInfo.groupUID);
+          socket.to(userInfo.groupUID).emit('getNewUserInfo', userInfo);
+        });
 
-            //Whenever someone connects this gets executed
-            io.on('connection', function (socket) {
+        socket.on('sendUserInfo', info => {
+          console.log(`Sending info to specific client: ${info.socketID}`);
+          socket.to(info.socketID).emit('getExistingUserInfo', info.userInfo);
+        });
 
-                console.log("Connection made.")
+        socket.on('selectLocation', selectedLocation => {
+          console.log(
+            `User selected location: ${JSON.stringify(selectedLocation)}`
+          );
+          io.in(selectedLocation.groupUID).emit('getSelectedLocation', selectedLocation);
+        });
 
-                socket.on('joinGroup', (userInfo) => {
-                    console.log(`User joined group: ${userInfo.groupUID}`);
-                    socket.join(userInfo.groupUID);
-                    socket.to(userInfo.groupUID).emit('getNewUserInfo', userInfo);
-                });
+        socket.on('sendSelectedLocation', selectedLocation => {
+          console.log(
+            `Sending info to specific client: ${selectedLocation.socketId}`
+          );
+          io.in(selectedLocation.groupUID).emit('getExistingSelectedLocation', selectedLocation);
+        });
 
-                socket.on('sendUserInfo', (info) => {
-                    console.log(`Sending info to specific client: ${info.socketID}`)
-                    socket.to(info.socketID).emit('getExistingUserInfo', info.userInfo);
-                })
-
-                //Whenever someone disconnects this piece of code executed
-                socket.on('disconnect', function () {
-                    console.log('A user disconnected');
-                });
-
-            });
-        }
-    }
-}
+        //Whenever someone disconnects this piece of code executed
+        socket.on('disconnect', function() {
+          console.log('A user disconnected');
+        });
+      });
+    },
+  };
+};
