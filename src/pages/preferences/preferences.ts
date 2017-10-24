5126e36974d637734a3158297a7448a08097ef63
Palacios95 <apala067@fiu.edu>
2017-10-23 22:53:01 -0400

    Added preferences

---
+++ src/pages/preferences/preferences.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core/';
+import { NavParams, ViewController } from 'ionic-angular';
+import {PreferenceOptions} from './preference-options';
+
+@Component({
+  selector: 'preferences',
+  templateUrl: 'preferences.html',
+})
+export class PreferencesPage {
+
+  preferenceOptions : PreferenceOptions;
+
+  constructor(public viewCtrl: ViewController, params: NavParams) {
+
+    this.preferenceOptions = {hasWifi: false, localDeals : false};
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  next()
+  {
+    this.viewCtrl.dismiss(this.preferenceOptions);
+  }
+}
